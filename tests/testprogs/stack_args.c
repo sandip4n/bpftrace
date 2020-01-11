efd594b413117b3862ec2713f08e81ac5e81a792
Sandipan Das <sandipan@linux.ibm.com>
2020-01-11 12:33:59 +0530

    tests: stack-args: Add multi-arch support
    For aarch64 and powerpc64, up to eight arguments can be
    passed using registers which makes this test fail as none
    of currently used arguments are on stack. This is fixed
    by adding extra arguments to test function in order to
    guarantee that the last two arguments are always on stack.
    
    Signed-off-by: Sandipan Das <sandipan@linux.ibm.com>

--- tests/testprogs/stack_args.c
+++ tests/testprogs/stack_args.c
@@ -1,10 +1,20 @@
 #define ARG(x) int (x) __attribute((unused))
 
 // Declare enough args that some are placed on the stack
-void too_many_args(ARG(a), ARG(b), ARG(c), ARG(d), ARG(e), ARG(f), ARG(g), ARG(h)) {}
+#if defined(__aarch64__) || defined(__powerpc64__)
+void too_many_args(ARG(a), ARG(b), ARG(c), ARG(d), ARG(e), ARG(f), ARG(g),
+                   ARG(h), ARG(i), ARG(j)) {}
+#else
+void too_many_args(ARG(a), ARG(b), ARG(c), ARG(d), ARG(e), ARG(f), ARG(g),
+                   ARG(h)) {}
+#endif
 
 int main(void)
 {
+#if defined(__aarch64__) || defined(__powerpc64__)
+  too_many_args(0, 0, 0, 1, 2, 4, 8, 16, 32, 64);
+#else
   too_many_args(0, 1, 2, 4, 8, 16, 32, 64);
+#endif
   return 0;
 }
